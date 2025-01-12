7e40e3bbec741f43f98ede90e82214e142a2ee53
netos23 <nikmoroz2002@outlook.com>
2025-01-13 00:34:39 +0300

    feature(task4): add task4 implementaton

--- src/Part4/Tasks.hs
+++ src/Part4/Tasks.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 module Part4.Tasks where
 
 import Util(notImplementedYet)
@@ -16,17 +17,47 @@ rlistToList lst =
 
 -- Реализуйте обратное преобразование
 listToRlist :: [a] -> ReverseList a
-listToRlist = notImplementedYet
+listToRlist = foldl (:<) REmpty
 
 -- Реализуйте все представленные ниже классы (см. тесты)
-instance Show (ReverseList a) where
-    showsPrec = notImplementedYet
-    show = notImplementedYet
-instance Eq (ReverseList a) where
-    (==) = notImplementedYet
-    (/=) = notImplementedYet
+instance Show a => Show (ReverseList a) where
+    showsPrec :: Show a => Int -> ReverseList a -> ShowS
+    showsPrec _ REmpty = showString ""
+    showsPrec d (init :< last) = showString $ "[" ++ showInner init ++ show last ++ "]" where 
+        showInner REmpty = ""
+        showInner (init :< last) = showInner init ++ show last ++ ","
+    show REmpty = "[]"
+    show (init :< last) = "[" ++ showInner init ++ show last ++ "]" where 
+        showInner REmpty = ""
+        showInner (init :< last) = showInner init ++ show last ++ ","
+            
+instance Eq a => Eq (ReverseList a) where
+    (==) :: Eq a => ReverseList a -> ReverseList a -> Bool
+    REmpty == REmpty = True
+    (init1 :< last1) == (init2 :< last2) = init1 == init2 && last1 == last2
+    _ == _ = False
+
+    (/=) :: Eq a => ReverseList a -> ReverseList a -> Bool
+    a /= b = not (a == b)
 instance Semigroup (ReverseList a) where
+    (<>) :: ReverseList a -> ReverseList a -> ReverseList a
+    REmpty <> a = a
+    a <> REmpty = a 
+    a <> (init :< last) = (a <> init) :< last
+
 instance Monoid (ReverseList a) where
+    mempty = REmpty
 instance Functor ReverseList where
+    fmap :: (a -> b) -> ReverseList a -> ReverseList b
+    fmap f REmpty = REmpty
+    fmap f (init:<last) = fmap f init :< f last
 instance Applicative ReverseList where
+    pure :: a -> ReverseList a
+    pure x = REmpty :< x
+    (<*>) :: ReverseList (a -> b) -> ReverseList a -> ReverseList b
+    REmpty <*> _ = REmpty
+    _ <*> REmpty = REmpty
+    (fs :< f) <*> xs = (fs <*> xs) <> (f <$> xs)
 instance Monad ReverseList where
+    REmpty >>= _ = REmpty
+    (xs :< x) >>= f = (xs >>= f) <> f x
